2b99df2a1d57742cb6d11cc3d9e21d03df69a83b
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-06-23 15:17:02 +0200

    feat: update msal config

--- frontend/src/auth/msalConfig.js
+++ frontend/src/auth/msalConfig.js
@@ -2,9 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: import.meta.env.VITE_MSAL_CLIENT_ID,
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: 'http://localhost:5173',
+    redirectUri: window.location.origin,
   },
   cache: { cacheLocation: 'sessionStorage' },
 };
-
-export const loginRequest = { scopes: ['openid', 'profile', 'email'] };
